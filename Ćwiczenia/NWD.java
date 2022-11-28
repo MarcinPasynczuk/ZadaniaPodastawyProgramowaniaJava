c99959acbd3caf76aa0f0e381217aff6c893a917
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-11-28 17:14:03 +0100

    Create NWD.java

---
+++ Ćwiczenia/NWD.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+Nwd(201202211, 145628);
+    }
+    public static void Nwd(int a, int b){
+        int e = b;
+        do {
+            System.out.printf("%-15s", a);
+            System.out.printf("%-15s", b);
+            System.out.printf("%-15s", a / b);
+            System.out.println();
+            b = a % b;
+            a = e;
+            e = b;
+
+        }
+        while (b > 0);
+    }
+}
